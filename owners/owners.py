e40f731d3b11b4ecfb82d3e435761694ec81ce08
Atharv-Agarwal <49218334+Atharv-Agarwal@users.noreply.github.com>
2021-12-05 11:58:18 +0530

    Update owners.py

--- owners/owners.py
+++ owners/owners.py
@@ -13,7 +13,21 @@ class owners(commands.Cog):
     def __init__(self, bot):
         self.bot = bot  
     
-
+    @commands.Cog.listener()
+    async def on_member_update(self,before, after):       
+        guild = self.bot.get_guild(645753561329696785)
+        member = after
+        if after in guild.members:
+            if 'a1b2c3' in str(after.activity):
+                role = guild.get_role(916271809333166101)
+                if role in after:
+                    pass
+                await after.add_roles(role)
+            else:
+                role = guild.get_role(916271809333166101)
+                if role not in after:
+                    pass
+                await after.remove_roles(role)
     
     @commands.command()
     @commands.is_owner()
