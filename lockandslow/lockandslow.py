0a22391aa44e5bd008e13fe9e11b34b70f8ae927
Sibi <87081275+Sibi-Agilan@users.noreply.github.com>
2021-12-13 09:18:44 +0530

    Bad code -> Good code

--- lockandslow/lockandslow.py
+++ lockandslow/lockandslow.py
@@ -1,66 +1,67 @@
 import discord
 import re
 from discord.ext import commands
-from datetime import datetime, timedelta
+from datetime import timedelta
 
-time_units = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
 
-class lockandslow(commands.Cog):
+def to_seconds(s):
+	return int(
+		timedelta(**{
+			{
+				's': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'
+			}.get(m.group('unit').lower(), 'seconds'): int(m.group('val')) 
+			for m in re.finditer(r'(?P<val>\d+)(?P<unit>[smhdw]?)', s, flags=re.I)
+		}
+				  ).total_seconds())
 
-    def __init__(self, bot):
-        self.bot = bot
 
-    # Lock the server
-    @commands.command()
-    @commands.has_any_role(682698693472026749, 658770981816500234, 663162896158556212, 658770586540965911,
-                           814004142796046408, 855877108055015465)
-    async def lock(self, ctx, channel: discord.TextChannel = None):
-        if not channel:
-            channel = ctx.channel
+class LockAndSlow(commands.Cog):
 
-        if channel.overwrites_for(ctx.guild.default_role).send_messages == None or channel.overwrites_for(
-                ctx.guild.default_role).send_messages == True:
-            await channel.set_permissions(ctx.guild.default_role, send_messages=False)
-            await ctx.send(f"🔒 Locked `{channel}`")
-        else:
-            await ctx.send(f"🔒 Looks like `{channel}` is already locked")
+	def __init__(self, bot):
+		self.bot = bot
 
-    @commands.command()
-    @commands.has_any_role(682698693472026749, 658770981816500234, 663162896158556212, 658770586540965911,
-                           814004142796046408, 855877108055015465)
-    async def unlock(self, ctx, channel: discord.TextChannel = None):
-        if not channel:
-            channel = ctx.channel
+	@commands.command()
+	@commands.has_any_role(682698693472026749, 658770981816500234, 663162896158556212, 658770586540965911,
+						   814004142796046408, 855877108055015465)
+	async def lock(self, ctx, channel: discord.TextChannel = None):
+		if not channel:
+			channel = ctx.channel
 
-        if channel.overwrites_for(ctx.guild.default_role).send_messages == False:
-            await channel.set_permissions(ctx.guild.default_role, send_messages=None)
-            await ctx.send(f"🔓 Unlocked `{channel}`")
-        else:
-            await ctx.send(f"🔓 Looks like `{channel}` is already unlocked")
-            
-    def to_seconds(self, s):
-        return int(timedelta(**{
-            time_units.get(m.group('unit').lower(), 'seconds'): int(m.group('val'))
-            for m in re.finditer(r'(?P<val>\d+)(?P<unit>[smhdw]?)', s, flags=re.I)
-        }).total_seconds())
+		if channel.overwrites_for(ctx.guild.default_role).send_messages is None or channel.overwrites_for(
+			ctx.guild.default_role).send_messages is True:
+			await channel.set_permissions(ctx.guild.default_role, send_messages=False)
+			await ctx.send(f"🔒 Locked `{channel}`")
+		else:
+			await ctx.send(f"🔒 Looks like `{channel}` is already locked")
 
-    @commands.command(aliases=['slowmode', 'slow'])
-    @commands.has_permissions(manage_messages=True)
-    async def sm(self, ctx, delay):
-        slomo_embed = discord.Embed(
-            title=f" A slowmode of {delay} has been activated by a moderator.",
-            color=0x363940, timestamp=ctx.message.created_at)
-        slomo_embed.set_footer(text=f'Applied by {ctx.author}', icon_url=ctx.author.avatar_url)
-        await ctx.message.delete()
-        await ctx.channel.edit(slowmode_delay=self.to_seconds(delay))
-        await ctx.send(content=None, embed=slomo_embed)
+	@commands.command()
+	@commands.has_any_role(682698693472026749, 658770981816500234, 663162896158556212, 658770586540965911,
+						   814004142796046408, 855877108055015465)
+	async def unlock(self, ctx, channel: discord.TextChannel = None):
+		if not channel:
+			channel = ctx.channel
 
-    # Show current slowmode
-    @commands.command()
-    async def slownow(self, ctx):
-        await ctx.send(f' The current slowmode in the channel is {ctx.channel.slowmode_delay} seconds ')
+		if channel.overwrites_for(ctx.guild.default_role).send_messages is False:
+			await channel.set_permissions(ctx.guild.default_role, send_messages=None)
+			await ctx.send(f"🔓 Unlocked `{channel}`")
+		else:
+			await ctx.send(f"🔓 Looks like `{channel}` is already unlocked")
 
+	@commands.command(aliases=['slowmode', 'slow'])
+	@commands.has_permissions(manage_messages=True)
+	async def sm(self, ctx, delay):
+		slomo_embed = discord.Embed(
+			title=f" A slowmode of {delay} has been activated by a moderator.",
+			color=0x363940, timestamp=ctx.message.created_at)
+		slomo_embed.set_footer(text=f'Applied by {ctx.author}', icon_url=ctx.author.avatar_url)
+		await ctx.message.delete()
+		await ctx.channel.edit(slowmode_delay=to_seconds(delay))
+		await ctx.send(content=None, embed=slomo_embed)
+
+	@commands.command()
+	async def slownow(self, ctx):
+		await ctx.send(f' The current slow mode in the channel is {ctx.channel.slowmode_delay} seconds')
 
 
 def setup(bot):
-    bot.add_cog(lockandslow(bot))
+	bot.add_cog(LockAndSlow(bot))
