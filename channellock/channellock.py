2334818b89a4d10647be8c73dd9210aa6bff19ef
Atharv-Agarwal <49218334+Atharv-Agarwal@users.noreply.github.com>
2021-12-16 11:02:09 +0530

    Update channellock.py

--- channellock/channellock.py
+++ channellock/channellock.py
@@ -3,8 +3,33 @@ import datetime
 from core import checks
 from core.models import PermissionLevel
 from discord.ext import commands
-from . import logger
-from logger import log
+
+first = True
+
+
+def clear_logs():
+	file = open('logs.txt', 'w')
+	file.write('# logs.txt file\n# Used to log errors & dev messages\n\nCurrent Date     | Current Time   | Log message\n\n')
+
+
+def log(text):
+	global first
+
+	dt = datetime.datetime.now()
+	dt_string = dt.strftime("Date: %d/%m/%Y | Time: %H:%M:%S")
+	file = open('logs.txt', 'r')
+	c_text = file.read()
+	file.close()
+
+	file = open('logs.txt', 'w')
+
+	if not first:
+		file.write(c_text + f'{dt_string} | {text}\n')
+	else:
+		clear_logs()
+		first = False
+
+	file.close()
 
 class Cog(commands.Cog):
     def __init__(self, bot):
