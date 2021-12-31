3df4dc221ffcce02de8f690dc9f654d898d713e4
Atharv-Agarwal <49218334+Atharv-Agarwal@users.noreply.github.com>
2021-12-31 02:42:51 +0000

    Update bfping.py

--- bfping/bfping.py
+++ bfping/bfping.py
@@ -2,60 +2,70 @@ import discord
 import asyncio
 from discord.ext import commands
 
-data = [
-    {'gaw': 672889430171713538},
-    {'partner': 793454145897758742},
-    {'heist': 684987530118299678},
-    {'ev': 684552219344764934},
-    {'friendly': 750908803704160268},
-    {'lot': 732949595633614938},
-    {'maf': 713898461606707273},
-]
-
-
-class BFping(commands.Cog):
+class bfping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot or not message.channel.permissions_for(message.author).manage_messages:
-            return
-
-        for x in data:
-            for key , value in x.items():
-                if message.content.startswith("??"+ key):
-                    msg = message.content[len("??"+ key)+ 1:] or '^_^'
-                    await message.channel.purge(limit=1)
-                    await message.channel.send(f'<@&{value}> {msg}')
-
-
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def gaw(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&672889430171713538> {messages}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def partner(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&793454145897758742> {messages}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def heist(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&684987530118299678> {messages}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def ev(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&684552219344764934> {messages}')   
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def friendly(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&750908803704160268> {messages}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def sale(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&724438185601663077> {messages}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def lot(self, ctx, *, message="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f' <@&732949595633614938> {message}')
+        
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def maf(self, ctx, *, messages="^_^"):
+        await ctx.channel.purge(limit=1)
+        await ctx.send(f'<@&713898461606707273> {messages}')
+        
     @commands.command()
     @commands.has_any_role(682698693472026749, 663162896158556212, 658770981816500234, 855877108055015465)
     async def esponsor(self, ctx, member: discord.Member):
         role = ctx.guild.get_role(787572079573598220)
-
         if role not in member.roles:
             await member.add_roles(role)
             await ctx.send("The role has been added")
             await asyncio.sleep(300)
             await member.remove_roles(role)
-
-            await ctx.send(f"The Event Sponsor role has has been removed from {member.mention}")
-
-            # allowed_mentions=discord.AllowedMentions.none() will not pass mentions into the message
-
+            await ctx.send("The Event Sponsor role has has been removed from {member.mention}")
         else:
-            """
-            This could be used in a situation where the bot turns off suddenly
-            due to an unexpected error and Farm Mail fails to remove the role.
-
-            The same command could be used when Farm Mail is back online to 
-            force - remove it.
-            """
-            await member.remove_roles(role)
-            await ctx.send("The role has been removed from them!")
-
+            await ctx.send("The user already has the role!")
 
 def setup(bot):
-    bot.add_cog(BFping(bot))
+    bot.add_cog(bfping(bot))
