61666ee04f573ced310dc60883c3cb1b4c88a375
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-14 10:36:19 +0530

    Update lockandslow.py

--- lockandslow/lockandslow.py
+++ lockandslow/lockandslow.py
@@ -60,7 +60,7 @@ class LockAndSlow(commands.Cog):
 		else:
 			await ctx.send(f"🔓 Looks like `{channel}` is already unlocked")
 
-	@commands.command(aliases=['slowmode', 'slow'])
+	@commands.command(aliases=['slowmodetest', 'slowtest'])
 	@commands.has_permissions(manage_messages=True)
 	async def smtest(self, ctx, delay):
 		slomo_embed = discord.Embed(
