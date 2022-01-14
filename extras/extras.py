bf939ffdad0271cf4a331f1205acfeb3602a32c8
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-14 16:51:34 +0530

    Update extras.py

--- extras/extras.py
+++ extras/extras.py
@@ -44,19 +44,19 @@ class Extras(commands.Cog):
 				alert = self.bot.get_channel(931503735052591124)
 				embed=discord.Embed(title="**Selling/Loan ad in #trade-buyers**", description=f"Someone posted a selling ad in <#759131412539768872> \n **[Jump to the ad]({message.jump_url})**", color=0xf70202)
 				react = await alert.send(embed=embed)
-				await react.add_reaction(<:farm_1wrongchannel:749882327458644058>)
+				await react.add_reaction("<:farm_1wrongchannel:749882327458644058>")
 		if message.channel.id == 685572368919298291:
 			if any(word in message.content.lower() for word in ('buying', 'buy' , 'loan')): 
 				alert = self.bot.get_channel(931503735052591124)
 				embed=discord.Embed(title="**Buying/Loan ad in #trade-sellers**", description=f"Someone posted a buying ad in <#685572368919298291> \n **[Jump to the ad]({message.jump_url})**", color=0xf70202)
 				react = await alert.send(embed=embed)
-				await react.add_reaction(<:farm_1wrongchannel:749882327458644058>)
+				await react.add_reaction("<:farm_1wrongchannel:749882327458644058>")
 		if message.channel.id == 784080892905652224:
 			if any(word in message.content.lower() for word in ('selling', 'sell' , 'loan','buying', 'buy')): 
 				alert = self.bot.get_channel(931503735052591124)
 				embed=discord.Embed(title="**Buying/Selling/Loan ad in #fight-ads**", description=f"Someone posted a buying/selling/loan ad in <#784080892905652224> \n **[Jump to the ad]({message.jump_url})**", color=0xf70202)
 				react = await alert.send(embed=embed)
-				await react.add_reaction(<:farm_1wrongchannel:749882327458644058>)
+				await react.add_reaction("<:farm_1wrongchannel:749882327458644058>")
 				
 	@commands.Cog.listener('on_message')
 	async def scammeralert(self, message: discord.Message):
