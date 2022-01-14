d7816d285e018ebb0d6f88d9987eb3c265c8fbab
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-14 10:52:50 +0530

    Update lockandslow.py

--- lockandslow/lockandslow.py
+++ lockandslow/lockandslow.py
@@ -31,15 +31,15 @@ class LockAndSlow(commands.Cog):
 		role2 = ctx.guild.get_role(663162896158556212)
 		role3 = ctx.guild.get_role(658770586540965911)
 		role4 = (role1 , role2 , role3)
-		
+
 		if any(role in ctx.author.roles for role in role4):
 			if channel.overwrites_for(ctx.guild.default_role).send_messages == None or channel.overwrites_for(ctx.guild.default_role).send_messages == True:
 				await channel.set_permissions(ctx.guild.default_role, send_messages=False)
 				await ctx.send(f"🔒 Locked `{channel}`")
 			else:
 				await ctx.send(f"🔒 Looks like `{channel}` is already locked")
-				
-		elif ctx.author.top_role == 855877108055015465:			
+
+		elif ctx.author.top_role.id == 855877108055015465:			
 			allowed_channels = [795879613393666048, 795709746501648384, 756552586248585368, 747853054329487500, 747184622386806824]
 			if ctx.channel.id in allowed_channels:
 				if channel.overwrites_for(ctx.guild.default_role).send_messages == None or channel.overwrites_for(ctx.guild.default_role).send_messages == True:
