d37f64d38630ada2aa15858b7044c01dc4d558b3
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2022-01-16 22:20:23 +0800

    Make sos work

--- sos/sos.py
+++ sos/sos.py
@@ -1,38 +1,83 @@
 import discord
-import asyncio
-from discord import Color
-from discord.utils import get
 from discord.ext import commands
-from dislash import slash_commands, ActionRow, Button, ButtonStyle
-class selfbutton(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-		if not hasattr(self.bot, "inter_client"):
-			slash_commands.InteractionClient(self.bot)
-	@commands.command()
-	async def sos(self,ctx, mem1: discord.Member, mem2: discord.Member):
-		SOS = ActionRow(
-			Button(style=ButtonStyle.green,label="Split",custom_id="split"),
-			Button(style=ButtonStyle.red,label="Steal",custom_id = "steal"),
-		)
-		
-		embed = discord.Embed(Title="Split or Steal?",description="ollla la oolaalala",color=Color.green())
-		msg = await ctx.send(embed=embed, components=[SOS])
-		def check(inter):
-			return inter.author == ctx.author
-		try:
-			inter = await msg.wait_for_button_click(check=check)
+from dislash import slash_commands, ActionRow, Button, ButtonStyle, MessageInteraction
 
-		except asyncio.exceptions.TimeoutError:
-			tembed= discord.Embed(Title="Timeout!",description="You didnt choose a color in time!",color=0x4ed0a4)
-			await msg.edit(embed=tembed,components=[SOS],disabled = True)
-			await asyncio.sleep(3)
-			await msg.delete
-			return
-		if inter.component.custom_id == "split":
-			await inter.respond(f"So you split?",ephemeral = True)
-		else:
-			await inter.respond(f"So you steal?", ephemeral = True)
-		
+class SplitOrSteal(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        if not hasattr(self.bot, "inter_client"):
+            slash_commands.InteractionClient(self.bot)
+    
+    @commands.command()
+    async def sos(self, ctx: commands.Context, player1: discord.Member, player2: discord.Member):
+        actions = ActionRow(
+            Button(style = ButtonStyle.green, label = "Split", custom_id = "split"),
+            Button(style = ButtonStyle.red, label = "Steal", custom_id = "steal"),
+        )
+
+        decision_embed = discord.Embed(title = "Split or Steal?", description = "You have 30 seconds to decide!", colour = 0x90EE90)
+
+        message = await ctx.send(embed = decision_embed, components = [actions])
+        on_click = message.create_click_listener(timeout = 30)
+
+        # choices for both players
+        choices = {player1: None, player2: None}
+
+        # player1's choice
+        @on_click.from_user(player1, cancel_others = False, reset_timeout = False)
+        async def response(inter: MessageInteraction):
+            choice = inter.component.custom_id
+            if choices[player1] is None:
+                choices[player1] = choice
+
+                if choice == "steal":
+                    return await inter.reply("You chose: `steal`, go big or go home!", ephemeral = True)
+                
+                if choice == "split":
+                    return await inter.reply("You chose: `split`, let's hope the other person does the same!", ephemeral = True)
+
+            return await inter.reply("You already chose, be patient!", ephemeral = True)
+
+        # player2's choice
+        @on_click.from_user(player2, cancel_others = False, reset_timeout = False)
+        async def response(inter: MessageInteraction):
+            choice = inter.component.custom_id
+            if choices[player2] is None:
+                choices[player2] = choice
+
+                if choice == "steal":
+                    return await inter.reply("You chose: `steal`, go big or go home!", ephemeral = True)
+                
+                if choice == "split":
+                    return await inter.reply("You chose: `split`, let's hope the other person does the same!", ephemeral = True)
+
+            return await inter.reply("You already chose, be patient!", ephemeral = True)
+
+        # acts as a check for both players
+        @on_click.no_checks(cancel_others = False, reset_timeout = False)
+        async def check(inter: MessageInteraction):
+            if inter.author not in (player1, player2):
+                return await inter.reply("These aren't your buttons! >:(", ephemeral = True)
+            
+            if None not in choices.values():
+                # kills the manager
+                on_click.kill()
+
+                result_embed = discord.Embed(
+                    title = "Split or Steal?",
+                    description = f"{player1.mention} chose **{choices[player1]}** \n"
+                                  f"{player2.mention} chose **{choices[player2]}**",
+                    colour = 0x90EE90
+                )
+                return await message.edit(embed = result_embed, components = [])
+
+        @on_click.timeout
+        async def on_timeout():
+            unresponsive_users = [user.mention for user, choice in choices.items() if choice is None]
+            
+            unresponsive_reply = " and ".join(unresponsive_users) + " did not reply in time!"
+            await message.edit(content = unresponsive_reply, embed = None, components = [])
+
 def setup(bot):
-	bot.add_cog(selfbutton(bot))
+    bot.add_cog(SplitOrSteal(bot))
