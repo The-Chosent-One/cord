4d38bf345732da00247b6eb9cb1edd99d1bf2206
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-24 09:15:33 +0530

    Create carl.py

---
+++ carl/carl.py
@@ -0,0 +1,81 @@
+import discord
+from discord.ext import commands
+from core import checks
+from core.models import PermissionLevel
+
+
+class Carl(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.coll = bot.plugin_db.get_partition(self)
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    async def addtrigger(self, ctx, trigger: str, title: str, description: str,
+                         channels: commands.greedy[discord.TextChannel] = None):
+        check = await self.coll.find_one({"trigger": trigger})
+        if check:
+            await ctx.send("Trigger already exists")
+        else:
+            if channel is None:
+                await self.coll.insert_one(
+                    {"trigger": trigger.lower(), "title": title, "description": description, "channel": "None"})
+                await ctx.send("Added trigger")
+            else:
+                chaid = [c.id for c in channels]
+                await self.coll.insert_one(
+                        {"trigger": trigger.lower(), "title": title, "description": description, "channel": chaid})
+
+    @commands.command(alias=["deltrigger"])
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    async def deletetrigger(self, ctx, trigger: str):
+        check = await self.coll.find_one({"trigger": trigger})
+        if check:
+            await self.coll.delete_one(check)
+            await ctx.send("Deleted trigger")
+        else:
+            await ctx.send("Trigger does not exist")
+
+    @commands.command()
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    async def trigger(self, ctx, trigger: str):
+        if trigger is None:
+            s = ""
+            fetchall = self.coll.find({})
+            async for x in fetchall:
+                trigger = x["trigger"]
+                s += f"{trigger} \n"
+                embed = discord.Embed(title="All triggers", description=s, color=0x00ff00)
+                await ctx.send(embed=embed)
+        else:
+            find = await self.coll.find_one({"trigger": trigger})
+            if find:
+                description = find["description"]
+                title = find["title"]
+                embed = discord.Embed(title=title, description=description, color=0x00ff00)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("Trigger does not exist, try `??trigger` to see available triggers")
+
+        @commands.Cog.listener('on_message')
+        async def triggered(self, message):
+            if message.author.bot:
+                return
+            if message.content.startswith('??'):
+                return
+            check = await self.coll.find_one({"trigger": message.content.lower()})
+            if not check:
+                return
+            else:
+                channel = check["channel"]
+                if channel == 'None':
+                    title = check["title"]
+                    description = check["description"]
+                    embed = discord.Embed(title=title, description=description)
+                    await message.channel.send(embed=embed)
+                else:
+                    if any(channel in message.chanel.id for channel in channel):
+                        title = check["title"]
+                        description = check["description"]
+                        embed = discord.Embed(title=title, description=description)
+                        await message.channel.send(embed=embed)
