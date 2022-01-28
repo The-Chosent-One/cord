14b5ee2e0e1c2c273fd63115452802f90b5f416f
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-28 07:40:51 +0530

    Update bfping.py

--- bfping/bfping.py
+++ bfping/bfping.py
@@ -2,101 +2,102 @@ import discord
 import asyncio
 from discord.ext import commands
 
-class bfping(commands.Cog):
+
+class BFPing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def gaw(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 658779198688722944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&672889430171713538> {messages}')
         else:
             await ctx.send('You can only use this command in <#658779198688722944>')
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def partner(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             partner = ctx.guild.get_role(790290355631292467)
             await ctx.author.remove_roles(partner)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 688431055489073180:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&793454145897758742> {messages}')
         else:
             await ctx.send('You can only use this command in <#688431055489073180>')
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def heist(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             heist = ctx.guild.get_role(723035638357819432)
             await ctx.author.remove_roles(heist)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 688581086078304260:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&684987530118299678> {messages}')
         else:
             await ctx.send('You can only use this command in <#688581086078304260>')
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def ev(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 709088851234258944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&684552219344764934> {messages}')
         else:
             await ctx.send('You can only use this command in <#709088851234258944>')
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def friendly(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 709088851234258944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&750908803704160268> {messages}')
         else:
             await ctx.send('You can only use this command in <#709088851234258944>')
-               
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def lot(self, ctx, *, message="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 732604674108030987:
             await ctx.channel.purge(limit=1)
             await ctx.send(f' <@&732949595633614938> {message}')
         else:
             await ctx.send('You can only use this command in <#732604674108030987>')
-        
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def maf(self, ctx, *, messages="^_^"):
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
-            return await ctx.send("Pretty sure you dont want to do that man")
+            return await ctx.send("Pretty sure you don't want to do that man")
         if ctx.channel.id == 756566417456889965:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&713898461606707273> {messages}')
         else:
             await ctx.send('You can only use this command in <#756566417456889965>')
-        
+
     @commands.command()
     @commands.has_any_role(682698693472026749, 663162896158556212, 658770981816500234, 855877108055015465)
     async def esponsor(self, ctx, member: discord.Member):
@@ -111,6 +112,7 @@ class bfping(commands.Cog):
         else:
             await member.remove_roles(role)
             await ctx.send("The role has been removed from them!")
-            
+
+
 def setup(bot):
-    bot.add_cog(bfping(bot))
+    bot.add_cog(BFPing(bot))
