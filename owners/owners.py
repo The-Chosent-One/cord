b6b4441d0f1f7dd275bd8222bb869bda31f7d48d
Cordila <49218334+Cordila@users.noreply.github.com>
2022-01-28 08:07:58 +0530

    Update owners.py

--- owners/owners.py
+++ owners/owners.py
@@ -1,4 +1,3 @@
-import re
 import discord
 from discord.ext import commands
 from core import checks
@@ -9,44 +8,47 @@ import psutil
 
 
 class Owners(commands.Cog):
-	def __init__(self, bot):
-		self.bot = bot
-				
-	@commands.Cog.listener()
-	async def on_thread_ready(self,thread,creator,category,initial_message):
-		msg = thread.genesis_message
-		if initial_message.content in ("hi","hii","hey","heyy","hello",):
-			msg = "Hey! Instead of just saying hi, please state your issue."
-			await thread.reply(msg)
+    def __init__(self, bot):
+        self.bot = bot
 
-	@commands.command()
-	@commands.is_owner()
-	async def usage(self, ctx):
-		await ctx.send(f'RAM memory % used: {psutil.virtual_memory()[2]}')
-		await ctx.send(f'The CPU % usage is: {psutil.cpu_percent(4)}')
+    @commands.Cog.listener()
+    async def on_thread_ready(self, thread, creator, category, initial_message):
+        msg = thread.genesis_message
+        if initial_message.content in ("hi", "hii", "hey", "heyy", "hello",):
+            msg = "Hey! Instead of just saying hi, please state your issue."
+            await thread.reply(msg)
 
-	@commands.command()
-	@commands.is_owner()
-	async def dm(self, ctx, user: discord.Member, *, message):
-		await user.send(f'Message from Bot Owner: {message}')
-		await ctx.channel.send("Sent the message")
+    @commands.command()
+    @commands.is_owner()
+    async def usage(self, ctx):
+        await ctx.send(f'RAM memory % used: {psutil.virtual_memory()[2]}')
+        await ctx.send(f'The CPU % usage is: {psutil.cpu_percent(4)}')
 
-	@commands.command(aliases=['logoff'])
-	@commands.is_owner()
-	async def reboot(self, ctx):
-		await ctx.send(f"Reboot the bot (Might crash)?? (y/n)")
-		msg = await self.bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel.id == ctx.channel.id)
-		if msg.content.lower() in ("y", "yes"):
-			await ctx.send("Ugh bye now")
-			await self.bot.close()
-		else:
-			await ctx.send("Okay bro wyd here then?")
-	
-	@commands.command(aliases=['ed'])
-	@checks.has_permissions(PermissionLevel.ADMIN)
-	async def enabledisable(self, ctx):
-		embed=discord.Embed(title="Enables/Disables", description="`??disableautoban | ??enableautoban` \n \n `??disabledecancer | ??enabledecancer` \n \n `??disableextras | ??enableextras` \n \n `??disablelock | ??enablelock` \n \n `??disableping | ??enableping` \n \n `??disableshortcut | ??enableshortcut` \n \n `??disablesnipe | ??enablesnipe` \n \n `??disablesuggest | ??enablesuggest` \n \n `??disabletyperacer | ??enabletyperacer` ")
-		await ctx.send(embed=embed)
+    @commands.command()
+    @commands.is_owner()
+    async def dm(self, ctx, user: discord.Member, *, message):
+        await user.send(f'Message from Bot Owner: {message}')
+        await ctx.channel.send("Sent the message")
+
+    @commands.command(aliases=['logoff'])
+    @commands.is_owner()
+    async def reboot(self, ctx):
+        await ctx.send(f"Reboot the bot (Might crash)?? (y/n)")
+        msg = await self.bot.wait_for("message",
+                                      check=lambda m: m.author == ctx.author and m.channel.id == ctx.channel.id)
+        if msg.content.lower() in ("y", "yes"):
+            await ctx.send("Ugh bye now")
+            await self.bot.close()
+        else:
+            await ctx.send("Okay bro wyd here then?")
+
+    @commands.command(aliases=['ed'])
+    @checks.has_permissions(PermissionLevel.ADMIN)
+    async def enabledisable(self, ctx):
+        embed = discord.Embed(title="Enables/Disables",
+                              description="`??disableautoban | ??enableautoban` \n \n `??disabledecancer | ??enabledecancer` \n \n `??disableextras | ??enableextras` \n \n `??disablelock | ??enablelock` \n \n `??disableping | ??enableping` \n \n `??disableshortcut | ??enableshortcut` \n \n `??disablesnipe | ??enablesnipe` \n \n `??disablesuggest | ??enablesuggest` \n \n `??disabletyperacer | ??enabletyperacer` ")
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
-	bot.add_cog(Owners(bot))
+    bot.add_cog(Owners(bot))
