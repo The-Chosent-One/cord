346040a6163912c518dfdc2724ef25a7af4c263c
Cordila <49218334+Cordila@users.noreply.github.com>
2022-02-01 08:39:08 +0530

    Update extras.py

--- extras/extras.py
+++ extras/extras.py
@@ -48,7 +48,7 @@ class Extras(commands.Cog):
                 react = await alert.send(embed=embed)
                 await react.add_reaction("<:farm_1wrongchannel:749882327458644058>")
         if message.channel.id == 685572368919298291:
-            if any(word in message.content.lower() for word in ('buying', 'buy', 'loan')):
+            if any(word in message.content.lower() for word in ('buying', 'loan')):
                 alert = self.bot.get_channel(931503735052591124)
                 embed = discord.Embed(title="**Buying/Loan ad in #trade-sellers**",
                                       description=f"Someone posted a buying ad in <#685572368919298291> \n **[Jump to the ad]({message.jump_url})**",
