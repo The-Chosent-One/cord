d8d4ecef5352978dff306e6c827bc50cb874f6da
Cordila <49218334+Cordila@users.noreply.github.com>
2022-02-10 13:21:16 +0530

    Update stickyroles.py

--- stickyroles/stickyroles.py
+++ stickyroles/stickyroles.py
@@ -1,1 +1,21 @@
+import discord
+from discord.ext import commands
 
+
+class SelfRoles(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.coll = bot.plugin_db.get_partition(self)
+
+    async def update_sticky(self, unique, role: discord.Role):
+        await self.coll.find_one_and_update({"unique": unique}, {"$push": {"role_id": role.id}}, upsert=True)
+
+    @commands.command()
+    async def addsticky(self, ctx, role: discord.Role):
+        """Adds a sticky role to the database"""
+        await self.update_sticky("1", role)
+        await ctx.send(f"Added {role.name} to the sticky roles")
+
+
+def setup(bot):
+    bot.add_cog(SelfRoles(bot))
