c87cddc09357bfed77089a7a8d0a70a18fcb8fa0
Cordila <49218334+Cordila@users.noreply.github.com>
2022-03-07 20:38:55 +0530

    Update owners.py
    Removed Usage, Reboot and some undone stuff

--- owners/owners.py
+++ owners/owners.py
@@ -4,43 +4,15 @@ from core import checks
 from core.models import PermissionLevel
 from core.thread import Thread
 
-import psutil
-
-
 class Owners(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.Cog.listener()
-    async def on_thread_ready(self, thread, creator, category, initial_message):
-        msg = thread.genesis_message
-        if initial_message.content in ("hi", "hii", "hey", "heyy", "hello",):
-            msg = "Hey! Instead of just saying hi, please state your issue."
-            await thread.reply(msg)
-
-    @commands.command()
-    @commands.is_owner()
-    async def usage(self, ctx):
-        await ctx.send(f'RAM memory % used: {psutil.virtual_memory()[2]}')
-        await ctx.send(f'The CPU % usage is: {psutil.cpu_percent(4)}')
 
     @commands.command()
     @commands.is_owner()
     async def dm(self, ctx, user: discord.Member, *, message):
         await user.send(f'Message from Bot Owner: {message}')
         await ctx.channel.send("Sent the message")
-
-    @commands.command(aliases=['logoff'])
-    @commands.is_owner()
-    async def reboot(self, ctx):
-        await ctx.send(f"Reboot the bot (Might crash)?? (y/n)")
-        msg = await self.bot.wait_for("message",
-                                      check=lambda m: m.author == ctx.author and m.channel.id == ctx.channel.id)
-        if msg.content.lower() in ("y", "yes"):
-            await ctx.send("Ugh bye now")
-            await self.bot.close()
-        else:
-            await ctx.send("Okay bro wyd here then?")
 
     @commands.command(aliases=['ed'])
     @checks.has_permissions(PermissionLevel.ADMIN)
