0caf00faa361bfa7ab9a59985cf5c37f92f1d3ce
Cordila <49218334+Cordila@users.noreply.github.com>
2022-03-12 12:09:13 +0530

    Update sos.py

--- sos/sos.py
+++ sos/sos.py
@@ -12,14 +12,14 @@ class Buttons(discord.ui.View):
     def __init__(self, *, timeout=30):
         super().__init__(timeout=timeout)
 
-    @discord.ui.button(label="Spilt", style=discord.ButtonStyle.green, custom_id="spilt")
-    async def spilt(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message("You chose: `split`, let's hope the other person does the same!",
-                                                ephemeral=True)
+        @discord.ui.button(label="Spilt", style=discord.ButtonStyle.green, custom_id="spilt")
+        async def spilt(self, button: discord.ui.Button, interaction: discord.Interaction):
+            await interaction.response.send_message("You chose: `split`, let's hope the other person does the same!",
+                                                    ephemeral=True)
 
-    @discord.ui.button(label="Steal", style=discord.ButtonStyle.red, custom_id="steal")
-    async def steal(self, button: discord.ui.Button, interaction: discord.Interaction):
-        await interaction.response.send_message("You chose: `steal`, go big or go home!", ephemeral=True)
+        @discord.ui.button(label="Steal", style=discord.ButtonStyle.red, custom_id="steal")
+        async def steal(self, button: discord.ui.Button, interaction: discord.Interaction):
+            await interaction.response.send_message("You chose: `steal`, go big or go home!", ephemeral=True)
 
     @commands.command()
     async def sos(self, ctx: commands.Context, player1: discord.Member, player2: discord.Member):
