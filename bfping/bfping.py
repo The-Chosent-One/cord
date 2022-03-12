bc4ebf1ace7d04a7928f3afa53ca118e5770db48
Cordila <49218334+Cordila@users.noreply.github.com>
2022-03-12 13:20:18 +0530

    Update bfping.py

--- bfping/bfping.py
+++ bfping/bfping.py
@@ -1,123 +1,34 @@
 import discord
 import asyncio
 from discord.ext import commands
-from core import checks
-from core.models import PermissionLevel
 
 time_units = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
 
 class BFPing(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.coll = bot.plugin_db.get_partition(self)
-
-    @commands.group(invoke_without_command=True)
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def pings(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send("You are probably looking for `??pings config`")
-
-    @pings.command(invoke_without_command=True)
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def config(self, ctx):
-        if ctx.invoked_subcommand is None:
-            await ctx.send("You are probably looking for `??pings config enable`/`??pings config disable`")
-
-    @config.command(name="enable")
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def config_enable(self, ctx, command, channel: discord.TextChannel = None):
-        if channel is None:
-            channel = ctx.channel
-
-        if command == "gaw":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'gaw_channel': channel.id}}, upsert=True)
-            await ctx.send(f"Enabled gaw in {channel.mention}")
-        elif command == "partner":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'partner_channel': channel.id}},
-                                                upsert=True)
-            await ctx.send(f"Enabled partner in {channel.mention}")
-        elif command == "heist":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'heist_channel': channel.id}},
-                                                upsert=True)
-            await ctx.send(f"Enabled heist in {channel.mention}")
-        elif command == "ev":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'ev_channel': channel.id}}, upsert=True)
-            await ctx.send(f"Enabled ev in {channel.mention}")
-        elif command == "friendly":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'friendly_channel': channel.id}},
-                                                upsert=True)
-            await ctx.send(f"Enabled friendly in {channel.mention}")
-        elif command == "lot":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'lot_channel': channel.id}}, upsert=True)
-            await ctx.send(f"Enabled lot in {channel.mention}")
-        elif command == "maf":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$push': {'maf_channel': channel.id}}, upsert=True)
-            await ctx.send(f"Enabled maf in {channel.mention}")
-        else:
-            await ctx.send("Invalid command")
-
-    @config.command(name="disable")
-    @checks.has_permissions(PermissionLevel.ADMIN)
-    async def config_disable(self, ctx, command, channel: discord.TextChannel = None):
-        if channel is None:
-            channel = ctx.channel
-
-        if command == "gaw":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'gaw_channel': channel.id}})
-            await ctx.send(f"Disabled gaw in {channel.mention}")
-        elif command == "partner":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'partner_channel': channel.id}})
-            await ctx.send(f"Disabled partner in {channel.mention}")
-        elif command == "heist":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'heist_channel': channel.id}})
-            await ctx.send(f"Disabled heist in {channel.mention}")
-        elif command == "ev":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'ev_channel': channel.id}})
-            await ctx.send(f"Disabled ev in {channel.mention}")
-        elif command == "friendly":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'friendly_channel': channel.id}})
-            await ctx.send(f"Disabled friendly in {channel.mention}")
-        elif command == "lot":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'lot_channel': channel.id}})
-            await ctx.send(f"Disabled lot in {channel.mention}")
-        elif command == "maf":
-            await self.coll.find_one_and_update({'Cog': 'BFPing'}, {'$pull': {'maf_channel': channel.id}})
-            await ctx.send(f"Disabled maf in {channel.mention}")
-        else:
-            await ctx.send("Invalid command")
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def gaw(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find[gaw_channel]
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 658779198688722944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&672889430171713538> {messages}')
         else:
 
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#658779198688722944>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def partner(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             partner = ctx.guild.get_role(790290355631292467)
             await ctx.author.remove_roles(partner)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find[partner_channel]
-        for chaid in channel
-
         if ctx.channel.id == 688431055489073180:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&793454145897758742> {messages}')
@@ -206,7 +117,8 @@ class BFPing(commands.Cog):
                 re.findall(r'(\d+)\s?([msh])?', text))
 
             if not 60 < seconds < 3600:
-                return await ctx.message.reply("Please keep the time between 1 minute and 1 hour.")
+                await ctx.message.reply("Please keep the time between 1 minute and 1 hour.")
+                raise BaseException
 
         role = ctx.guild.get_role(787572079573598220)
         if role not in member.roles:
@@ -219,7 +131,6 @@ class BFPing(commands.Cog):
         else:
             await member.remove_roles(role)
             await ctx.send("The role has been removed from them!")
-
 
 def setup(bot):
     bot.add_cog(BFPing(bot))
