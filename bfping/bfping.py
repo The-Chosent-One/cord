22b2a918eaa8155d9a4e6b990fc1b85c15e09825
Cordila <49218334+Cordila@users.noreply.github.com>
2022-03-12 13:05:03 +0530

    Update bfping.py

--- bfping/bfping.py
+++ bfping/bfping.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
 
+time_units = {'s': 'seconds', 'm': 'minutes', 'h': 'hours', 'd': 'days', 'w': 'weeks'}
 
 class BFPing(commands.Cog):
     def __init__(self, bot):
@@ -16,7 +17,7 @@ class BFPing(commands.Cog):
         if ctx.invoked_subcommand is None:
             await ctx.send("You are probably looking for `??pings config`")
 
-    @pings.group(invoke_without_command=True)
+    @pings.command(invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.ADMIN)
     async def config(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -94,7 +95,7 @@ class BFPing(commands.Cog):
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
         find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['gaw_channel']
+        channel = find[gaw_channel]
         for chaid in channel:
             chai = self.bot.get_channel(chaid)
             c += f"{chai.mention}, "
@@ -102,8 +103,9 @@ class BFPing(commands.Cog):
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&672889430171713538> {messages}')
         else:
+
             await ctx.send(f'You can only use this command in {c}')
-            
+
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def partner(self, ctx, *, messages="^_^"):
@@ -113,119 +115,104 @@ class BFPing(commands.Cog):
             await ctx.author.remove_roles(partner)
             return await ctx.send("Pretty sure you don't want to do that man")
         find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['partner_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention},"
-        if ctx.channel.id in channel:
+        channel = find[partner_channel]
+        for chaid in channel
+
+        if ctx.channel.id == 688431055489073180:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&793454145897758742> {messages}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#688431055489073180>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def heist(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             heist = ctx.guild.get_role(723035638357819432)
             await ctx.author.remove_roles(heist)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['heist_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 688581086078304260:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&684987530118299678> {messages}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#688581086078304260>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def ev(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['ev_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 709088851234258944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&684552219344764934> {messages}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#709088851234258944>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def friendly(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['friendly_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 709088851234258944:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&750908803704160268> {messages}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#709088851234258944>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def lot(self, ctx, *, message="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['lot_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 732604674108030987:
             await ctx.channel.purge(limit=1)
             await ctx.send(f' <@&732949595633614938> {message}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#732604674108030987>')
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def maf(self, ctx, *, messages="^_^"):
-        c = ""
         if ctx.message.raw_role_mentions or '@everyone' in ctx.message.content or '@here' in ctx.message.content:
             gwm = ctx.guild.get_role(855877108055015465)
             await ctx.author.remove_roles(gwm)
             return await ctx.send("Pretty sure you don't want to do that man")
-        find = await self.coll.find_one({'Cog': 'BFPing'})
-        channel = find['maf_channel']
-        for chaid in channel:
-            chai = self.bot.get_channel(chaid)
-            c += f"{chai.mention}, "
-        if ctx.channel.id in channel:
+        if ctx.channel.id == 756566417456889965:
             await ctx.channel.purge(limit=1)
             await ctx.send(f'<@&713898461606707273> {messages}')
         else:
-            await ctx.send(f'You can only use this command in {c}')
+            await ctx.send('You can only use this command in <#756566417456889965>')
+
+    @staticmethod
+    def to_seconds(s):
+        return int(timedelta(**{
+            time_units.get(m.group('unit').lower(), 'seconds'): int(m.group('val'))
+            for m in re.finditer(r'(?P<val>\d+)(?P<unit>[smhdw]?)', s, flags=re.I)
+        }).total_seconds())
 
     @commands.command()
     @commands.has_any_role(682698693472026749, 663162896158556212, 658770981816500234, 855877108055015465)
-    async def esponsor(self, ctx, member: discord.Member):
+    async def esponsor(self, ctx, member: discord.Member, seconds):
+        try:
+            text = seconds
+            seconds = sum(
+                int(num) * {'h': 60 * 60, 'm': 60, 's': 1, ' ': 1}[weight if weight else 's'] for num, weight in
+                re.findall(r'(\d+)\s?([msh])?', text))
+
+            if not 60 < seconds < 3600:
+                return await ctx.message.reply("Please keep the time between 1 minute and 1 hour.")
+
         role = ctx.guild.get_role(787572079573598220)
         if role not in member.roles:
             await member.add_roles(role)
             await ctx.send("The role has been added")
-            await asyncio.sleep(300)
+            await asyncio.sleep(time)
             if role in member.roles:
                 await member.remove_roles(role)
                 await ctx.send(f"The Event Sponsor role has has been removed from {member.mention}")
