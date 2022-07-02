a73794efb06eda038497ca933de2890dcc21d565
Cordila <49218334+Cordila@users.noreply.github.com>
2022-07-02 11:53:04 +0530

    Update reminders.py

--- reminders/reminders.py
+++ reminders/reminders.py
@@ -46,7 +46,7 @@ class Reminders(commands.Cog):
             text = time
             seconds = sum(
                 int(num) * {'d': 60*60*24, 'h': 60 * 60, 'm': 60, 's': 1, ' ': 1}[weight if weight else 's'] for num, weight in
-                re.findall(r'(\d+)\s?([msh])?', text))
+                re.findall(r'(\d+)\s?([mshd])?', text))
             if seconds < 10:
                 await ctx.message.reply('I can\'t remind you under 10 seconds. Maybe improve your memory?')
                 return BaseException
