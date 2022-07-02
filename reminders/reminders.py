a245b9b5d9749b11098393ddf9c839f2cae17621
Cordila <49218334+Cordila@users.noreply.github.com>
2022-07-02 10:05:31 +0530

    Update reminders.py

--- reminders/reminders.py
+++ reminders/reminders.py
@@ -82,7 +82,8 @@ class Reminders(commands.Cog):
         for reminder in reminders:
             try:
                 user = self.bot.get_user(reminder['user_id'])
-                embed = discord.Embed(title=f"**Reminder!**", description=f"You asked to be reminded of \"{reminder['message']}\" [here](reminder['msg_link']) ", color=0x10ea64)
+                link = reminder['msg_link']
+                embed = discord.Embed(title=f"**Reminder!**", description=f"You asked to be reminded of \"{reminder['message']}\" [here](link) ", color=0x10ea64)
                 await user.send(embed=embed)
                 await self.coll.delete_one({"_id": reminder["_id"]})
                 fetch = await self.coll.find().sort('time', 1).to_list(1)
