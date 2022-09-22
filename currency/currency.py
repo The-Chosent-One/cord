b9eddef01f127a8040d034f84be82a13d4fd2eda
The-Chosent-One <57739806+The-Chosent-One@users.noreply.github.com>
2022-09-22 22:18:36 +0800

    Another feex

--- currency/currency.py
+++ currency/currency.py
@@ -11,25 +11,23 @@ from time import time
 AMOUNT_MAP = {"k": "*1000", "m": "*1000000", "b": "*1000000000"}
 AMOUNT_REGEX = re.compile(rf"^-?\d+(?:(?:\.\d+)?[{''.join(AMOUNT_MAP)}]|(?:\.\d+)?e\d+)?$")
 
-
 class Amount(commands.Converter):
     async def convert(self, ctx: commands.Context, argument: str) -> int:
         match = AMOUNT_REGEX.search(argument)
 
         if match is None:
             raise commands.BadArgument("Converting failed")
-
+        
         amount = match.group(0)
         for suffix, replacement in AMOUNT_MAP.items():
             amount = amount.replace(suffix, replacement)
-
+        
         res: int | float = compile(amount, "", "eval").co_consts[0]
 
         if isinstance(res, float) and not res.is_integer():
             raise commands.BadArgument("Converting failed")
-
+        
         return int(res)
-
 
 class Currency(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -37,12 +35,12 @@ class Currency(commands.Cog):
         collection: motor_asyncio.AsyncIOMotorCollection = bot.plugin_db.get_partition(self)
         self.currency_handler = CurrencyHandler(self.bot, collection)
         self.emoji = "<:farm_zzcash:927739006899335188>"
-
+    
     async def cog_check(self, ctx: commands.Context) -> bool:
-        if ctx.channel.id != 789809104738189342:
+        if ctx.author.id != 531317158530121738:
             return False
         return True
-
+    
     async def change_cash(self, target: discord.Member, cash: int) -> discord.Embed:
         updated_cash = await self.currency_handler.update_cash(target, cash=cash)
 
@@ -57,36 +55,36 @@ class Currency(commands.Cog):
     async def addcash(self, ctx, target: discord.Member, cash: Amount):
         if cash < 0:
             return await ctx.reply("Use `??removecash` instead")
-
+        
         await ctx.send(embed=await self.change_cash(target, cash))
 
     @addcash.error
-    async def update_error(self, ctx: commands.Context, error) -> None:
+    async def addcash_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.reply("Correct usage:\n```??addcash <member> <amount>```")
         if isinstance(error, commands.MemberNotFound):
             return await ctx.reply("You keyed in an invalid user or the user is not in the guild")
         if isinstance(error, commands.BadArgument):
             return await ctx.reply("That's not an integer")
-
+        
         traceback.print_exception(type(error), error, error.__traceback__)
 
     @commands.command()
     async def removecash(self, ctx, target: discord.Member, cash: Amount):
         if cash < 0:
             return await ctx.reply("Use `??addcash` instead")
-
+        
         await ctx.send(embed=await self.change_cash(target, -cash))
 
     @removecash.error
-    async def update_error(self, ctx: commands.Context, error) -> None:
+    async def removecash_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.reply("Correct usage:\n```??removecash <member> <amount>```")
         if isinstance(error, commands.MemberNotFound):
             return await ctx.reply("You keyed in an invalid user or the user is not in the guild")
         if isinstance(error, commands.BadArgument):
             return await ctx.reply("That's not an integer")
-
+        
         traceback.print_exception(type(error), error, error.__traceback__)
 
     @commands.command()
@@ -102,57 +100,53 @@ class Currency(commands.Cog):
         await ctx.send(embed=embed)
 
     @setcash.error
-    async def update_error(self, ctx: commands.Context, error) -> None:
+    async def setcash_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.BadArgument):
             return await ctx.reply("That's not an integer")
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.reply("Correct usage:\n```??setcash <member> <amount>```")
         if isinstance(error, commands.MemberNotFound):
             return await ctx.reply("You keyed in an invalid user or the user is not in the guild")
-
+        
         traceback.print_exception(type(error), error, error.__traceback__)
-
+    
     @commands.command(aliases=["give"])
     async def share(self, ctx: commands.Context, target: discord.Member, amount: Amount) -> None:
         if amount < 0:
             return await ctx.reply("That won't work :>")
-
+        
         if amount == 0:
             return await ctx.reply("Then why..?")
-
+        
         if amount >= 2147483648:
             return await ctx.reply("🧢 no way you have that much")
-
+        
         cash = await self.currency_handler.get_cash(ctx.author)
 
-        tax = ceil(0.15 * amount)
+        tax = ceil(0.15*amount)
         required = amount + tax
 
         if required > cash:
-            embed = discord.Embed(title="You're too broke ⍨",
-                                  description=f"You only have {self.emoji} **{cash:,}**\n(And cannot pay {self.emoji} **{amount:,}** + {self.emoji} **{tax:,}** in taxes)",
-                                  colour=0xeb3434)
+            embed = discord.Embed(title="You're too broke ⍨", description=f"You only have {self.emoji} **{cash:,}**\n(And cannot pay {self.emoji} **{amount:,}** + {self.emoji} **{tax:,}** in taxes)", colour=0xeb3434)
             embed.set_footer(text="Tax amount is 15%")
             return await ctx.reply(embed=embed)
 
         await self.currency_handler.update_cash(target, cash=amount)
         await self.currency_handler.update_cash(ctx.author, cash=-required)
 
-        embed = discord.Embed(title="Shared coins!",
-                              description=f"Paid {self.emoji} **{amount:,}** + {self.emoji} **{tax:,}** (in taxes) to {target.mention}!",
-                              colour=0x49eb34)
+        embed = discord.Embed(title="Shared coins!", description=f"Paid {self.emoji} **{amount:,}** + {self.emoji} **{tax:,}** (in taxes) to {target.mention}!", colour=0x49eb34)
         embed.set_footer(text="Tax amount is 15%")
         await ctx.reply(embed=embed)
 
     @share.error
-    async def update_error(self, ctx: commands.Context, error) -> None:
+    async def share_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.MissingRequiredArgument):
             return await ctx.reply("Correct usage:\n`??share <member> <amount>`")
         if isinstance(error, commands.MemberNotFound):
             return await ctx.reply("You keyed in an invalid user or the user is not in the guild")
         if isinstance(error, commands.BadArgument):
             return await ctx.reply("That's not an integer")
-
+        
         traceback.print_exception(type(error), error, error.__traceback__)
 
     @commands.command(alias=["bal"])
@@ -161,27 +155,24 @@ class Currency(commands.Cog):
 
         embed = discord.Embed(title=f"{ctx.author}'s balance", description=f"Cash: {self.emoji} **{cash:,}**")
         await ctx.reply(embed=embed)
-
+    
     @commands.command()
     async def income(self, ctx: commands.Context) -> None:
         next_income_time = await self.currency_handler.get_next_income_time(ctx.author)
-
+        
         if next_income_time is not None and time() < next_income_time:
-            embed = discord.Embed(title="Income cooldown",
-                                  description=f"You can claim your income in <t:{next_income_time}:R>")
+            embed = discord.Embed(title="Income cooldown", description=f"You can claim your income in <t:{next_income_time}:R>")
             return await ctx.reply(embed=embed)
+        
+        # level income, booster income, cash income
+        income_groups = [0, 0, 0]
 
-        give_level_income = give_booster_income = give_cash_income = 0
-
-        for give_income, income_data in zip(
-                [give_level_income, give_booster_income, give_cash_income],
-                [LEVEL_INCOMES, BOOSTER_INCOMES, CASH_INCOMES]
-        ):
+        for index, income_data in enumerate([LEVEL_INCOMES, BOOSTER_INCOMES, CASH_INCOMES]):
             for role_id, income in income_data.items():
                 if ctx.author._roles.has(role_id):
-                    give_income = max(income, give_income)
-
-        total_income = give_level_income + give_booster_income + give_cash_income
+                    income_groups[index] = max(income, income_groups[index])
+        
+        total_income = sum(income_groups)
 
         if total_income == 0:
             embed = discord.Embed(title="Income collection", description="You don't have any income to claim :(")
@@ -189,13 +180,17 @@ class Currency(commands.Cog):
 
         await self.currency_handler.update_cash(ctx.author, total_income)
 
-        next_time = round(time()) + 604800  # amount of seconds in a week
+        next_time = round(time()) + 604800 # amount of seconds in a week
         await self.currency_handler.update_income_time(ctx.author, next_time)
 
-        embed = discord.Embed(title="Income collection",
-                              description=f"You collected {self.emoji} **{total_income:,}**!")
+        embed = discord.Embed(title="Income collection", description=f"You collected {self.emoji} **{total_income:,}**!")
         await ctx.reply(embed=embed)
 
+    @income.error
+    async def income_error(self, ctx: commands.Context, error) -> None:
+        traceback.print_exception(type(error), error, error.__traceback__)
+    
+        
 
 async def setup(bot):
     await bot.add_cog(Currency(bot))
