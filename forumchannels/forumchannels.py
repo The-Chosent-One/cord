20d993eda0533fb0e4d571f6fe2c16d33599c094
Cordila <49218334+Cordila@users.noreply.github.com>
2022-09-24 12:43:41 +0530

    Update forumchannels.py

--- forumchannels/forumchannels.py
+++ forumchannels/forumchannels.py
@@ -8,36 +8,25 @@ class ForumChannels(commands.Cog):
 
     @commands.Cog.listener(name="on_raw_thread_update")
     async def forum_channel_archived(self, payload):
-        print('event triggered')
         data = payload.data
-        print(data)
-        if (
-            payload.data["parent_id"] == 1019806662766379160
-            and payload.data["archived"] == True
-            and payload.data["archiver_id"] == payload.data["owner_id"]
-        ):
-            print('true')
+        if data["archived"] and data["parent_id"] == "1019806662766379160":
             channel = self.bot.get_channel(payload.data["id"])
-            member = self.bot.get_user(payload.data["owner_id"])
-            await channel.edit(archived=False)
-            await channel.send(
-                f"{member.mention} \n You cannot archive your own thread. It has been unarchived and will be automatically archived after 1 hour of inactivity."
-            )
-        if (
-            payload.data["parent_id"] == 1019806662766379160
-            and payload.data["archived"] == True
-            and payload.data["archiver_id"] != payload.data["owner_id"]
-        ):
-            channel = self.bot.get_channel(payload.data["id"])
-            member = self.bot.get_user(payload.data["owner_id"])
-            if member is None:
-                return
-            await channel.set_permissions(
-                member.guild.default_role, send_messages=False
-            )
-            await channel.send(
-                f"Thread has been archived and locked due to inactivity. Please create a new thread if you wish to continue using the bot"
-            )
+            if channel.archiver_id == int(data["owner_id"]):
+                await channel.edit(archived=False)
+                member = self.bot.get_user(payload.data["owner_id"])
+                await channel.send(
+                    f"{member.mention} \n You cannot archive your own thread. It has been unarchived and will be automatically archived after 1 hour of inactivity."
+                )
+            if channel.archiver_id != int(data["owner_id"]):
+                member = self.bot.get_user(payload.data["owner_id"])
+                if member is None:
+                    return
+                await channel.set_permissions(
+                    member.guild.default_role, send_messages=False
+                )
+                await channel.send(
+                    f"Thread has been archived and locked due to inactivity. Please create a new thread if you wish to continue using the bot"
+                )
 
 
 async def setup(bot):
