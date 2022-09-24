93465d857a355134b5ce711bd76976e3d1cfcb7f
Cordila <49218334+Cordila@users.noreply.github.com>
2022-09-24 13:10:20 +0530

    Update forumchannels.py

--- forumchannels/forumchannels.py
+++ forumchannels/forumchannels.py
@@ -24,7 +24,7 @@ class ForumChannels(commands.Cog):
                 member = self.bot.get_user(int(payload.data["owner_id"]))
                 if member is None:
                     return
-                await channel.edit(locked=True, archived=True, reason="Archived")
+                await channel.edit(locked=True, reason="Archived")
                 await channel.send(
                     f"Thread has been archived and locked due to inactivity. Please create a new thread if you wish to continue using the bot"
                 )
