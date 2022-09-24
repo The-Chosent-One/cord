053d1bfdca141fe671137c9f9103788a5878bd5a
Cordila <49218334+Cordila@users.noreply.github.com>
2022-09-24 12:46:08 +0530

    Update forumchannels.py

--- forumchannels/forumchannels.py
+++ forumchannels/forumchannels.py
@@ -9,16 +9,16 @@ class ForumChannels(commands.Cog):
     @commands.Cog.listener(name="on_raw_thread_update")
     async def forum_channel_archived(self, payload):
         data = payload.data
-        if data["archived"] and data["parent_id"] == "1019806662766379160":
-            channel = self.bot.get_channel(payload.data["id"])
+        if data["archived"] == True and data["parent_id"] == "1019806662766379160":
+            channel = self.bot.get_channel(int(payload.data["id"]))
             if channel.archiver_id == int(data["owner_id"]):
                 await channel.edit(archived=False)
-                member = self.bot.get_user(payload.data["owner_id"])
+                member = self.bot.get_user(int(payload.data["owner_id"]))
                 await channel.send(
                     f"{member.mention} \n You cannot archive your own thread. It has been unarchived and will be automatically archived after 1 hour of inactivity."
                 )
             if channel.archiver_id != int(data["owner_id"]):
-                member = self.bot.get_user(payload.data["owner_id"])
+                member = self.bot.get_user(int(payload.data["owner_id"]))
                 if member is None:
                     return
                 await channel.set_permissions(
