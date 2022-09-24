341010d7fb9a5e9fde5a9f7c4f2dcb7b55152bcf
Cordila <49218334+Cordila@users.noreply.github.com>
2022-09-24 13:38:24 +0530

    Update forumchannels.py

--- forumchannels/forumchannels.py
+++ forumchannels/forumchannels.py
@@ -13,12 +13,21 @@ class ForumChannels(commands.Cog):
             data["thread_metadata"]["archived"] == True
             and data["parent_id"] == "1019806662766379160"
         ):
-            channel = await self.bot.fetch_channel(int(payload.data["id"]))
-            print(channel.id)
-            print(channel.archiver_id)
-            print(channel.archiver)
-            print(data["owner_id"])
-
+            channel = await self.bot.get_channel(int(payload.data["id"]))
+            if channel.archiver_id == int(data["owner_id"]):
+                await channel.edit(archived=False)
+                await channel.send(
+                    "You cannot archive your own thread. It has been unarchived and will be automatically archived after 1 hour of inactivity."
+                )
+            if channel.archiver_id != int(data["owner_id"]):
+                member = self.bot.get_user(int(payload.data["owner_id"]))
+                if member is None:
+                    return
+                await channel.edit(archived=False)
+                await channel.edit(locked=True, archived=True, reason="Archived")
+                await channel.send(
+                    f"Thread has been archived and locked due to inactivity. Please create a new thread if you wish to continue using the bot"
+                )
 
 
 async def setup(bot):
